72384c818d4d4ad079cf773d249b406c0e985946
Austin Belknap <dabelknap@wisc.edu>
2014-03-19 16:34:00 -0500

    Remove UCT stuff

--- python/emulation_cfi.py
+++ python/emulation_cfi.py
@@ -11,8 +11,6 @@ import FWCore.ParameterSet.Config as cms
 
 
 from Configuration.StandardSequences.RawToDigi_Data_cff import *
-from L1Trigger.UCT2015.Lut import *
-from L1Trigger.UCT2015.uct2015L1ExtraParticles_cfi import *
 
 
 # Modify the HCAL TPGs according to the proposed HTR modification.  If the HCAL
@@ -27,8 +25,8 @@ hackHCALMIPs = cms.EDProducer(
 
 uctDigis = cms.EDProducer(
     "L1RCTProducer",
-    #hcalDigis = cms.VInputTag(cms.InputTag("hcalDigis")),
-    hcalDigis = cms.VInputTag(cms.InputTag("hackHCALMIPs")),
+    hcalDigis = cms.VInputTag(cms.InputTag("hcalDigis")),
+    #hcalDigis = cms.VInputTag(cms.InputTag("hackHCALMIPs")),
     useEcal = cms.bool(True),
     useHcal = cms.bool(True),
     ecalDigis = cms.VInputTag(cms.InputTag("ecalDigis:EcalTriggerPrimitives")),
@@ -36,58 +34,6 @@ uctDigis = cms.EDProducer(
     getFedsFromOmds = cms.bool(False),
     queryDelayInLS = cms.uint32(10),
     queryIntervalInLS = cms.uint32(100)#,
-)
-
-UCT2015EClusterProducer = cms.EDProducer(
-    "UCT2015EClusterProducer",
-    debug = cms.bool(False),
-    puCorrect = cms.bool(True),
-    puETMax = cms.uint32(7),
-    eClusterSeed = cms.uint32(5),
-    # Transparency correction calibration
-    # calib_v4 = 2012 data.  Use calib_v1 (ideal) for MC.
-    ecalCalibration = cms.vdouble(eg_calib_v4),
-    ecalLSB = cms.double(0.5),
-    ecalDigis = cms.VInputTag(cms.InputTag("ecalDigis:EcalTriggerPrimitives"))
-)
-
-UCT2015Producer = cms.EDProducer(
-    "UCT2015Producer",
-    puCorrect = cms.bool(True),
-    useUICrho = cms.bool(True),
-    useHI = cms.bool(False),
-    # All of these uint32 thresholds are in GeV.
-    puETMax = cms.uint32(7),
-    regionETCutForHT = cms.uint32(5),
-    regionETCutForMET = cms.uint32(0),
-    minGctEtaForSums = cms.uint32(4),
-    maxGctEtaForSums = cms.uint32(17),
-    jetSeed = cms.uint32(5),
-    egtSeed = cms.uint32(5),
-    relativeIsolationCut = cms.double(10.),
-    relativeJetIsolationCut = cms.double(0.2),
-    switchOffTauIso= cms.double(60),
-    egammaLSB = cms.double(1.0), # This has to correspond with the value from L1CaloEmThresholds
-    regionLSB = RCTConfigProducers.jetMETLSB,
-)
-
-UCTStage1BProducer = cms.EDProducer(
-    "UCTStage1BProducer",
-    puCorrect = cms.bool(True),
-    egSeed = cms.uint32(5),
-    tauSeed = cms.uint32(5),
-    regionalHoECut = cms.double(0.05),
-    egRelativeRgnIsolationCut = cms.double(0.1),
-    egRelativeJetIsolationCut = cms.double(0.1),
-    egRelativeEMRgnIsolationCut = cms.double(0.1),
-    egRelativeEMJetIsolationCut = cms.double(0.1),
-    tauRelativeRgnIsolationCut = cms.double(0.1),
-    tauRelativeJetIsolationCut = cms.double(0.1),
-    tauRelativeEMRgnIsolationCut = cms.double(0.1),
-    tauRelativeEMJetIsolationCut = cms.double(0.1),
-    egLSB = cms.double(0.5),
-    tauLSB = cms.double(1.0),  # This has to correspond with the value from L1CaloEmThresholds
-    regionLSB = RCTConfigProducers.jetMETLSB
 )
 
 from L1Trigger.L1ExtraFromDigis.l1extraParticles_cfi import *
@@ -105,9 +51,6 @@ uctEmulatorStep = cms.Sequence(
     hackHCALMIPs
     # Now make UCT and L1 objects
     * uctDigis
-    * UCT2015EClusterProducer
-    * UCT2015Producer
-    * UCTStage1BProducer
 #    * l1extraParticles
 #    * uct2015L1ExtraParticles
 )
