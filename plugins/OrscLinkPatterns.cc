33aa9f81f18c6f9e5e7fa6e58c79aa6b4c5bbfe3
Austin Belknap <dabelknap@wisc.edu>
2014-03-26 09:07:57 -0500

    Use CrateLinks class instead of OrscLinks

--- plugins/OrscLinkPatterns.cc
+++ plugins/OrscLinkPatterns.cc
@@ -32,7 +32,7 @@
 #include "DataFormats/L1CaloTrigger/interface/L1CaloEmCand.h"
 #include "DataFormats/L1CaloTrigger/interface/L1CaloRegionDetId.h"
 
-#include "L1Trigger/oRSCPatterns/interface/OrscLinks.h"
+#include "L1Trigger/CaloLinks/include/CrateLinks.h"
 
 
 class OrscLinkPatterns : public edm::EDAnalyzer {
@@ -50,6 +50,9 @@ class OrscLinkPatterns : public edm::EDAnalyzer {
     edm::Handle<L1CaloEmCollection> newEMCands;
 
     std::ofstream outfile;
+
+    void addRegion(CrateLinks& crateLink, const L1CaloRegion &reg);
+    void addEM(CrateLinks& crateLink, const L1CaloEmCand &cand);
 };
 
 
@@ -76,18 +79,18 @@ OrscLinkPatterns::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetu
   iEvent.getByLabel("uctDigis", newEMCands);
 
   // Create OrscLink objects for each crate
-  OrscLinks link_crate[18];
+  CrateLinks link_crate[18];
 
   // Load each region into the OrscLink object of its crate
   for (L1CaloRegionCollection::const_iterator newRegion = newRegions->begin();
       newRegion != newRegions->end(); newRegion++) {
-    link_crate[newRegion->rctCrate()].addRegion(*newRegion);
+    addRegion(link_crate[newRegion->rctCrate()], *newRegion);
   }
 
   // Load each EM Candidate into the OrscLink object of its crate
   for (L1CaloEmCollection::const_iterator egtCand = newEMCands->begin();
       egtCand != newEMCands->end(); egtCand++) {
-    link_crate[egtCand->rctCrate()].addEM(*egtCand);
+    addEM(link_crate[egtCand->rctCrate()], *egtCand);
   }
 
   // For each OrscLink crate object, print the optical link layouts into a text
@@ -95,8 +98,8 @@ OrscLinkPatterns::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetu
   for (int i = 0; i < 18; ++i) {
     link_crate[i].populate_link_tables();
 
-    std::vector<uint32_t> link1 = link_crate[i].link_values(1);
-    std::vector<uint32_t> link2 = link_crate[i].link_values(2);
+    std::vector<uint8_t> link1 = link_crate[i].link_values(1);
+    std::vector<uint8_t> link2 = link_crate[i].link_values(2);
 
     // Print the contents of each link into the text file
     outfile << std::setw(2) << std::uppercase << std::setfill('0');
@@ -126,6 +129,81 @@ OrscLinkPatterns::beginJob() {
 void
 OrscLinkPatterns::endJob() {
   outfile.close();
+}
+
+
+void
+OrscLinkPatterns::addRegion(CrateLinks& crateLink, const L1CaloRegion &reg) {
+  unsigned card   = reg.rctCard();
+  unsigned region = reg.rctRegionIndex();
+  unsigned et     = reg.et();
+
+  bool tau        = reg.tauVeto();
+  bool mip        = reg.mip();
+  bool overflow   = reg.overFlow();
+  bool finegrain  = reg.fineGrain();
+
+  bool isHF       = reg.isHf();
+
+
+  if (isHF) {
+    crateLink.set_HFFg(region, finegrain & 0x1);
+
+    for (int i = 0; i < 8; ++i) {
+      crateLink.set_HFEt(region, i, et & 0x1);
+      et >>= 1;
+    }
+  }
+  else {
+    crateLink.set_RCOf(card, region, overflow & 0x1);
+    crateLink.set_RCTau(card, region, tau & 0x1);
+    crateLink.set_RCHad(card, region, mip & 0x1);
+
+    // separate the 10 region bits
+    for (int i = 0; i < 10; ++i) {
+      crateLink.set_RCEt(card, region, i, et & 0x1);
+      et >>= 1;
+    }
+  }
+}
+
+/**
+ * Add a L1CaloEmCand and parse the bits
+ */
+void
+OrscLinkPatterns::addEM(CrateLinks& crateLink, const L1CaloEmCand &cand) {
+  unsigned index = cand.index();
+  unsigned card = cand.rctCard();
+  unsigned region = cand.rctRegion();
+  unsigned rank = cand.rank();
+  bool iso = cand.isolated();
+
+  // Separate and store the 6 rank bits, 3 card bits, region bit, for iso and
+  // non-iso EM candidates.
+  if (iso) {
+    for (int i = 0; i < 6; ++i) {
+      crateLink.set_NEEt(index, i, rank & 0x1);
+      rank >>= 1;
+    }
+
+    crateLink.set_NEPos(index, 0, region & 0x1);
+    for (int i = 1; i < 4; ++i) {
+      crateLink.set_NEPos(index, i, card & 0x1);
+      card >>= 1;
+    }
+  }
+  else {
+    for (int i = 0; i < 6; ++i) {
+      crateLink.set_IEEt(index, i, rank & 0x1);
+      rank >>= 1;
+    }
+
+    crateLink.set_IEPos(index, 0, region & 0x1);
+    for (int i = 1; i < 4; ++i) {
+      crateLink.set_IEPos(index, i, card & 0x1);
+      card >>= 1;
+    }
+  }
 }
 
 
